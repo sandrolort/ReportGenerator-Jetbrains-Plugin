e2fe67c0c594f2460cf85d10ebe97b0225ff6b10
Sandro Lortkipanidze <lortkipandize.sandro@proton.me>
2024-09-02 23:29:24 +0400

    Modernizations and Optimizations in the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,59 +1,36 @@
-import org.gradle.internal.classpath.Instrumented.systemProperty
-
 plugins {
-    id("java")
     id("org.jetbrains.kotlin.jvm") version "2.0.0"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.0.0"
 }
 
-group = "ge.kvisha"
-version = "1.0-SNAPSHOT"
+group = "org.kvisha"
+version = "1.0"
+
+val PublishToken: String by project
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
 
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+        intellijDependencies()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2024.2.3")
-    type.set("RD") // Target IDE Platform
-
-    plugins.set(listOf())
+intellijPlatform {
+    projectName = "report-generator"
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-}
-
-tasks {
-    systemProperty("javafx.verbose", "true")
-
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
-    }
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("242.*")
-    }
-
-    buildSearchableOptions {
-        enabled = false
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+    intellijPlatform {
+        rider("2024.2.3")
+        instrumentationTools()
     }
 }
